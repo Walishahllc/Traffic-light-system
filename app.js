5a7872ff5dbe11ce735bd7870347f885aaa59a85
Wali Shah <walishahllc@gmail.com>
2024-03-29 14:55:35 +0500

    Traffic-light

---
+++ app.js
@@ -0,0 +1,14 @@
+let color = "red";
+
+//Traffic Light System
+if (color === "red") {
+    console.log("Stop! light color is red");
+}
+
+if (color === "yellow") {
+    console.log("Slow Down! light color is yellow");
+}
+
+if (color === "green") {
+    console.log("Go! light color is green");
+}
